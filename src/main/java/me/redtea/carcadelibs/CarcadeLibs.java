f73b3254b68915d3065009125edff255e08e8d39
iRedTea <red__tea@outlook.com>
2022-07-28 16:26:50 +0300

    add java project

---
+++ src/main/java/me/redtea/carcadelibs/CarcadeLibs.java
@@ -0,0 +1,17 @@
+package me.redtea.carcadelibs;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CarcadeLibs extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
